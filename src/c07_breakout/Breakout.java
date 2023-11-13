65635c8ccfad485865adddf1d9ee2b18122e035c
Hyun <53088084+gusdndl@users.noreply.github.com>
2023-11-13 16:42:33 +0900

    Update Breakout.java

--- src/c07_breakout/Breakout.java
+++ src/c07_breakout/Breakout.java
@@ -1,20 +1,7 @@
 package c07_breakout;
 
-import javax.swing.JFrame;
-
-public class Breakout extends JFrame {
-	public Breakout() {
-		add(new Board());
-		setTitle("Breakout");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(Commons.WIDTH, Commons.HEIGHT);
-		setLocationRelativeTo(null);
-		setIgnoreRepaint(true);
-		setResizable(false);
-		setVisible(true);
-	}
-
+public class Breakout {
 	public static void main(String[] args) {
-		new Breakout();
+		new BreakoutFrame();
 	}
 }
